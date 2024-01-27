5d4df5a75e838628c8ae96b4f2ae6ab02e89feff
Prakash <prakashgmalagi@gmail.com>
2024-01-27 14:48:49 +0530

    3rd commit

---
+++ ATB5x01.java
@@ -0,0 +1,5 @@
+public class ATB5x01 {
+    public static void main(String[] args) {
+        System.out.println("Hey This is new file");
+    }
+}
---
+++ DemoPrograms/DemoPrgm01.java
@@ -0,0 +1,8 @@
+package DemoPrograms;
+
+public class DemoPrgm01 {
+    public static void main(String[] args) {
+        System.out.println("hello1");
+        System.out.println("hello 2");
+    }
+}
