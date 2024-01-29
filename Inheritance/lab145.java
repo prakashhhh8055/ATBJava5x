17ea568438237dc037c6345732bee8595b593095
Prakash <prakashgmalagi@gmail.com>
2024-01-29 19:03:09 +0530

    cdsdsssds COmzxzmizt

--- Inheritance/lab145.java
+++ Inheritance/lab145.java
@@ -8,10 +8,12 @@ public class lab145 {
 //        Programming p2 = new Java();
 //        // Java j2 = new Programming();
 
-//        Java j = new Java();
-//        j.car();
-//        j.bhk3();
-//        j.author = "Jasmes Gaosling";
+        Java j = new Java();
+        Java j1=new Java("Oops");
+        j1.printInfo();
+        j.car();
+        j.bhk3();
+        j.author = "Jasmes Gaosling";
 
 
       //  Python p = new Python();
